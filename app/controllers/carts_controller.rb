0161a481416af7a85f9b8e84c03eebb35b785f15
Brian <bcoleman4@hotmail.co.uk>
2013-03-21 10:01:54 +0000

    Revert "Mid way through Chapter 11"
    This reverts commit 503ca0e32cd4638218110dfa89a3bb9c57b15e5b.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -83,8 +83,8 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to store_url }
-      format.json { head :ok }
+      format.html { redirect_to carts_url, notice: 'Your cart is currently empty' }
+      format.json { head :no_content }
     end
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,7 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   
+  
   has_many :line_items, dependent: :destroy
 
   def add_product(product_id)
