45d68f45793710eeaf129feec94b0629ec0c413a
Brian <bcoleman4@hotmail.co.uk>
2013-03-27 15:48:32 +0000

    Up to Chapter 15 customised
    Added a login button from catalogue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,12 @@ class SessionsController < ApplicationController
   skip_before_filter :authorize
   
   def new
+
   end
 
   def create
     user = User.find_by_name(params[:name])
+    
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
